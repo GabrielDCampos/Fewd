95cef1419bafffeb481226c6e0435d9301e011e9
GabrielDCampos <gabox10@live.com.mx>
2016-07-09 14:31:43 -0700

    new stuf to submit

---
+++ likes-and-comments/script.js
@@ -0,0 +1,21 @@
+var link = document.querySelector(".like-link");
+var form = document.querySelector("#new-comment");
+
+link.addEventListener("click", like);
+form.addEventListener("submit", comment);
+
+function like(event) {
+  event.preventDefault();
+
+  // Your code for like goes here
+}
+
+function comment(event) {
+  event.preventDefault();
+  var newelement= document.createElement("div");
+  newelement.setAttribute("class","comments");
+  newelement.textContent=comentBody.value;
+  newelement.appendChild(comments)
+
+  // Your code for comments goes here
+}
