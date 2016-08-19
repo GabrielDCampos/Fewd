d4b74e8ea13caa3baa240502c401b437b114d64e
GabrielDCampos <gabox10@live.com.mx>
2016-08-18 18:37:24 -0700

    final project (but not)

---
+++ Final-project-memoryGame/memory.js/basic.js
@@ -0,0 +1,66 @@
+
+
+
+var firstCard;
+var secondCard;
+
+var cardImgs = ["flags-imgs/1.png", "flags-imgs/2.png", "flags-imgs/3.png", "flags-imgs/4.png"]
+cardImgs = shuffle(cardImgs.concat(cardImgs));
+
+creatingCards();
+function creatingCards(){
+  for (var i = 0; i < 8; i++) {
+    $("<div>").addClass("card cardFaceDown").appendTo(".main-board")
+      .css("background-image", "url(" + cardImgs[i] + ")")
+      .on("click", flipCard)
+
+  }
+
+}
+
+function shuffle(array) {
+ var currentIndex = array.length, temporaryValue, randomIndex;
+
+ // While there remain elements to shuffle...
+ while (0 !== currentIndex) {
+   // Pick a remaining element...
+   randomIndex = Math.floor(Math.random() * currentIndex);
+   currentIndex -= 1;
+
+   // And swap it with the current element.
+   temporaryValue = array[currentIndex];
+   array[currentIndex] = array[randomIndex];
+   array[randomIndex] = temporaryValue;
+ }
+
+ return array;
+}
+
+function flipCard(){
+  if (firstCard) {
+
+    secondCard=$(this)
+    secondCard.removeClass("cardFaceDown");
+
+    if (firstCard.css("background-image") === secondCard.css("background-image")) {
+
+
+
+    }else{
+      setTimeout(wrong,700);
+
+
+    }
+
+  } else {
+    firstCard = $(this)
+    firstCard.removeClass("cardFaceDown");
+    }
+
+}
+function wrong(){
+  firstCard.addClass("cardFaceDown") && secondCard.addClass("cardFaceDown")
+  console.log("rong");
+  firstCard = null;
+  secondCard = null;
+}
